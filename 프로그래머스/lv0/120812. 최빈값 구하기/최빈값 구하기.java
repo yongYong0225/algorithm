8d345a683a474a9601288605863356c83014b777
jaeYong <118151866+yongYong0225@users.noreply.github.com>
2023-02-13 00:06:18 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,40 +1,32 @@
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        int arrayMax=0;
-        for(int i=0; i<array.length; i++) {    
-            if(array[i]>arrayMax) {
-                arrayMax=array[i];
+
+        int[] count=new int[1000];
+
+        for(int j=0; j<array.length; j++) {
+            count[array[j]]++;                    //새로 만든 배열에 1을 더해줌
+        }
+
+        int max=0;
+        for(int m=0; m<count.length; m++) {     //새로만든 배열의 최대값을 구함
+            if(count[m]>max) {
+                max=count[m];              //새로만든 배열의 m번째 요소가 최대값이라면
+                answer=m;                       //매개변수로 들어온 배열의 m 이 최빈값
             }
         }
-        
-        int[] count=new int[arrayMax+1];        
-        
-        for(int j=0; j<array.length; j++) {        
-            count[array[j]]++;                    
-        }
-        
-        int countMax=0;
-        for(int m=0; m<count.length; m++) {     
-            if(count[m]>countMax) {
-                countMax=count[m];              
-                answer=m;                       
-            }
-        }                                            
-        
-        int rep=0;
-        for(int n=0; n<count.length; n++) {    
-            if(countMax==count[n]) {            
-                rep++;                          
+
+        int dup = 0;
+
+        for(int n=0; n<count.length; n++) {     //그 최대값이 중복으로 있는지를 확인    
+            if(max==count[n]) {            //중복이 없다면 rep는 1,
+                dup++;                          //중복이 있다면 reo는 2이상
             }
         }
-        
-        if(rep!=1) {
+
+        if(dup!=1) {
             answer=-1;
         } 
-        
-        
-        
-        return answer;
-    }
+            return answer;
+}
 }
