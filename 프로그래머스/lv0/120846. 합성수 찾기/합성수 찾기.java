90abd204c6f2b19d29dea49e4d137c2d9d622619
jaeYong <118151866+yongYong0225@users.noreply.github.com>
2023-03-07 21:38:12 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++){
+            int cnt = 0;
+            for(int j=1; j<=i; j++){
+                if(i%j==0){
+                    cnt += 1;
+                }
+                
+                }
+            if(cnt>=3){
+                    answer +=1;
+            }
+        }
+        
+        return answer;
+    }
+}
